13e5bf34fbd02173054bc57dc2ba20d90671ef05
Resham Bharti <reshambharti145@gmail.com>
2023-04-19 10:23:00 +0530

    Add files via upload

---
+++ Final/Main.java
@@ -0,0 +1,21 @@
+package Final;
+
+import javax.swing.JFrame;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        JFrame obj=new JFrame();
+        Gameplay gamePlay = new Gameplay();
+
+        obj.setBounds(400, 150, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+
+    }
+
+}
